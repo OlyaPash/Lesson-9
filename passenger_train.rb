a9d3cb261fa345945ea58dda2d9bdf7dbf4b21b8
OlyaPash <olya.pash.rb@gmail.com>
2022-05-19 12:15:12 +0500

    more improvements

--- passenger_train.rb
+++ passenger_train.rb
@@ -1,6 +1,8 @@
 class PassengerTrain < Train
+  
   validate :number, :presence
   validate :type, :presence
+  validate :number, :format, /(.|\d){3}-*(.|\d){2}/
 
   def initialize(type = "passenger", number)
     @number = number
